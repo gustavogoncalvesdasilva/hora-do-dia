d34ad2f7056ee8bfa0610b4d5d15719b61a5a563
Gustavo_GS <131196509+gustavogoncalvesdasilva@users.noreply.github.com>
2023-09-12 08:22:54 -0300

    adicionando mensagem

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function carregar(){
     msg = document.querySelector('#msg')
     img = document.querySelector('#imagem')
+    sau = document.querySelector('#sau')
     data = new Date()
     hora = data.getHours()
     //hora = 
@@ -10,19 +11,23 @@ function carregar(){
     if (hora >= 0 && hora < 12)
     {
         //bom dia
+        sau.innerHTML = 'Bom Dia'
         img.src = 'manha.png'
         document.body.style.background = '#6b4f27'
     }
     else if (hora >=12 && hora <= 18) 
     {
         //boa tarde
+        sau.innerHTML = 'Boa Tarde'
         img.src = 'tarde.png'
         document.body.style.background = '#ef5401'
     }
     else {
         //boa noite 
+        sau.innerHTML = 'Boa Noite'
         img.src = 'noite.png'
         document.body.style.background = '#0c1719'
     }
+
 }
 
